ea7507a78aa22f4722af350c5f30ff21698795b8
JonathanMouraa <270908@unifio.edu.br>
2025-08-20 01:42:07 +0000

    Aula 19/08/25

--- src/main/java/app/PrimeiraClasse.java
+++ src/main/java/app/PrimeiraClasse.java
@@ -3,5 +3,12 @@ package app;
 public class PrimeiraClasse{
     public static void main (String [] args){
         System.out.println("Olá Gradle!!!");
+
+        Aluno a1 = new Aluno();
+        a1.setId(1);
+        a1.setNome("Marco");
+        a1.setIdade(45);
+
+        System.out.println(a1.getNome());
     }
 }
