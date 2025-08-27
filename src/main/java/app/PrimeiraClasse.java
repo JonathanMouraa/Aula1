a5e4681f6cae84619902f41b608487c8e60e8efa
JonathanMouraa <270908@unifio.edu.br>
2025-08-27 01:42:12 +0000

    Aula

--- src/main/java/app/PrimeiraClasse.java
+++ src/main/java/app/PrimeiraClasse.java
@@ -1,6 +1,9 @@
 package app;
 
-public class PrimeiraClasse{
+import java.util.List;
+import java.util.ArrayList;
+
+public class PrimeiraClasse {
     public static void main (String [] args){
         System.out.println("Olá Gradle!!!");
 
@@ -10,5 +13,23 @@ public class PrimeiraClasse{
         a1.setIdade(45);
 
         System.out.println(a1.getNome());
-    }
+
+        Tarefa t1 = new Tarefa();
+        t1.setDescricao("Estudar Estruturas de Dados");
+        t1.setConcluido(true);
+
+
+        Tarefa t2 = t1;
+        t2.setDescricao("Exemplo de Tarefa");
+
+        System.out.println(t1.getDescricao());
+         System.out.println(t2.getDescricao());
+
+
+         List<Atividade> agenda = new ArrayList<Atividade>();
+         agenda.add(t1);
+         Compromisso c1 = new Compromisso();
+        agenda.add(c1);
+
+        }
 }
